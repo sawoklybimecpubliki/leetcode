0d011a09e2ba3751494f7c46fa3d7d06177deb0c
AlexadnrShlemin <sawoklybimec@gmail.com>
2025-05-01 18:17:45 +0300

    solved v1

--- 2962 count-subarrays-where-max-element-appears-at-least-k-times/main.go
+++ 2962 count-subarrays-where-max-element-appears-at-least-k-times/main.go
@@ -55,5 +55,6 @@ func countSubarrays(nums []int, k int) int64 {
 }
 
 func main() {
-	fmt.Println("answer: ", countSubarrays([]int{1, 3, 2, 3, 3}, 2))
+	fmt.Println("answer: ", countSubarrays([]int{61, 23, 38, 23, 56, 40, 82, 56, 82, 82, 82, 70, 8, 69, 8,
+		7, 19, 14, 58, 42, 82, 10, 82, 78, 15, 82}, 2))
 }
