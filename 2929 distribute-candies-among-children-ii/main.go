3c20af3e258b5dbcf92687d08b4a90105746aa82
AlexadnrShlemin <sawoklybimec@gmail.com>
2025-06-01 15:29:33 +0300

    solved

---
+++ 2929 distribute-candies-among-children-ii/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func distributeCandies(n, limit int) int64 {
+	total := int64(0)
+
+	for i := 0; i <= min(limit, n); i++ {
+		minJ := max(0, n-i-limit)
+		maxJ := min(limit, n-i)
+		if minJ <= maxJ {
+			total += int64(maxJ - minJ + 1)
+		}
+	}
+
+	return total
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func main() {
+	fmt.Println(distributeCandies(5, 2))
+}
