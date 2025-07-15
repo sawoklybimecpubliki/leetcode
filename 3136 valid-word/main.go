bee3a5d5d286b484c6176f4931b1fb2e6a9cc29b
AlexadnrShlemin <sawoklybimec@gmail.com>
2025-07-15 14:10:12 +0300

    solved

---
+++ 3136 valid-word/main.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+func isValid(word string) bool {
+	var flagCh, flagDi, flagVo, flagCo = false, true, false, false
+	c := 0
+	for _, l := range word {
+		c++
+		if !flagCh && c > 2 {
+			flagCh = true
+		}
+		if (l < '0' || l > '9') && (l < 'A' || l > 'z') {
+			flagDi = false
+			break
+		}
+		if !flagCo && (l >= 'A' && l <= 'z') {
+			flagCo = isConstant(l)
+		}
+		if !flagVo && (l >= 'A' && l <= 'z') {
+			flagVo = !isConstant(l)
+		}
+	}
+	return flagCh && flagCo && flagDi && flagVo
+}
+
+func isConstant(char rune) bool {
+	for _, vowel := range "aeiouAEIOU" {
+		if char == vowel {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	fmt.Println(isValid("UuE6"))
+}
