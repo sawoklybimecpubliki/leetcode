b113171d3ce0a97d7cc36010b4ed3b32c43d13bf
AlexadnrShlemin <sawoklybimec@gmail.com>
2025-10-04 19:31:41 +0300

    solved v2

--- 11 container-with-most-water/main.go
+++ 11 container-with-most-water/main.go
@@ -3,32 +3,55 @@ package main
 import "fmt"
 
 func maxArea(height []int) int {
-	var maxHeight, square int
 
-	for i := len(height) - 1; i > 0; i-- {
+	left := 0
+	right := len(height) - 1
+	maxArea := 0
 
-		if maxHeight < height[i] {
-			maxHeight = height[i]
-			for j := 0; j < i; j++ {
-				if height[i] > height[j] {
+	for left < right {
+		width := right - left
+		h := height[left]
+		if height[right] < h {
+			h = height[right]
+		}
+		area := h * width
+		if area > maxArea {
+			maxArea = area
+		}
 
-					if (i-j)*height[j] > square {
-						square = (i - j) * height[j]
-					}
+		if height[left] < height[right] {
+			left++
+		} else {
+			right--
+		}
+	}
+	return maxArea
+	/*	var maxHeight, square int
 
-				} else {
+		for i := len(height) - 1; i > 0; i-- {
 
-					if (i-j)*height[i] > square {
-						square = (i - j) * height[i]
+			if maxHeight < height[i] {
+				maxHeight = height[i]
+				for j := 0; j < i; j++ {
+					if height[i] > height[j] {
+
+						if (i-j)*height[j] > square {
+							square = (i - j) * height[j]
+						}
+
+					} else {
+
+						if (i-j)*height[i] > square {
+							square = (i - j) * height[i]
+						}
+
 					}
 
 				}
-
 			}
-		}
 
-	}
-	return square
+		}
+		return square*/
 }
 
 func main() {
