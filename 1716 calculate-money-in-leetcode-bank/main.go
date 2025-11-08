49aead5fdf999fe0f6b4b564b292ba491eb24c9b
AlexadnrShlemin <sawoklybimec@gmail.com>
2025-11-08 13:14:11 +0300

    solved

---
+++ 1716 calculate-money-in-leetcode-bank/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func totalMoney(n int) int {
+	var money, week int
+	for n > 7 {
+		money += (7*(7+1))/2 + 7*week
+		week++
+		n -= 7
+	}
+	money += (n*(n+1))/2 + n*week
+	return money
+}
+
+func main() {
+	fmt.Println(totalMoney(4))
+}
