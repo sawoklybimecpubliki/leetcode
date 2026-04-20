51eba1c735a398a69bcf7a5d14674e614d4bbcd2
AlexadnrShlemin <sawoklybimec@gmail.com>
2026-04-20 15:33:34 +0300

    solved v2

--- 2078 two-furthest-houses-with-different-colors/main.go
+++ 2078 two-furthest-houses-with-different-colors/main.go
@@ -4,6 +4,21 @@ import "fmt"
 
 func maxDistance(colors []int) int {
 	var distance int
+	n := len(colors)
+	for i := n - 1; i >= 0; i-- {
+		if colors[0] != colors[i] && distance < i {
+			distance = i
+		}
+	}
+	for i := 0; i < n; i++ {
+		if colors[n-1] != colors[i] && distance < n-1-i {
+			distance = n - 1 - i
+		}
+	}
+	return distance
+}
+
+/*
 	for i := 0; i < len(colors)-1; i++ {
 		for j := i + 1; j < len(colors); j++ {
 			if colors[i] != colors[j] {
@@ -13,10 +28,8 @@ func maxDistance(colors []int) int {
 			}
 		}
 	}
-	return distance
-}
-
+*/
 func main() {
-	colors := []int{1, 8, 3, 8, 3}
+	colors := []int{4, 4, 4, 11, 4, 4, 11, 4, 4, 4, 4, 4}
 	fmt.Println(maxDistance(colors))
 }
