c33874686e7852f3450ffd17d5d456e370bb332c
AlexadnrShlemin <sawoklybimec@gmail.com>
2026-04-27 17:05:37 +0300

    solved

---
+++ 1391 check-if-there-is-a-valid-path-in-a-grid/main.go
@@ -0,0 +1,82 @@
+package main
+
+import "fmt"
+
+// -
+func hasValidPath(grid [][]int) bool {
+	m, n := len(grid), len(grid[0])
+
+	dirs := [4][2]int{
+		{0, -1},
+		{0, 1},
+		{-1, 0},
+		{1, 0},
+	}
+
+	streetDirs := map[int][]int{
+		1: {0, 1},
+		2: {2, 3},
+		3: {0, 3},
+		4: {1, 3},
+		5: {0, 2},
+		6: {1, 2},
+	}
+
+	opposite := [4]int{1, 0, 3, 2}
+
+	visited := make([][]bool, m)
+	for i := range visited {
+		visited[i] = make([]bool, n)
+	}
+
+	type Pair struct {
+		r, c int
+	}
+
+	queue := make([]Pair, 0)
+	queue = append(queue, Pair{0, 0})
+	visited[0][0] = true
+	head := 0
+
+	for head < len(queue) {
+		cur := queue[head]
+		head++
+
+		r, c := cur.r, cur.c
+		if r == m-1 && c == n-1 {
+			return true
+		}
+
+		for _, d := range streetDirs[grid[r][c]] {
+			nr := r + dirs[d][0]
+			nc := c + dirs[d][1]
+
+			if nr < 0 || nr >= m || nc < 0 || nc >= n || visited[nr][nc] {
+				continue
+			}
+
+			nextType := grid[nr][nc]
+			ok := false
+			for _, nd := range streetDirs[nextType] {
+				if nd == opposite[d] {
+					ok = true
+					break
+				}
+			}
+
+			if ok {
+				visited[nr][nc] = true
+				queue = append(queue, Pair{nr, nc})
+			}
+		}
+	}
+
+	return false
+}
+
+func main() {
+	grid := [][]int{
+		{2, 4, 3},
+		{6, 5, 2}}
+	fmt.Println(hasValidPath(grid))
+}
